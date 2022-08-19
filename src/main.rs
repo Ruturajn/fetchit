37d1c6d49401cc3f5fcc6734fd9ed9fc662e61ee
Ruturajn <nanotiruturaj@gmail.com>
2022-08-20 00:51:28 +0530

    `add`: ascii art to output, and box it

--- src/main.rs
+++ src/main.rs
@@ -13,44 +13,52 @@ fn main() {
         Ok(x) => x,
         Err(_) => String::from("Unknown"),
     };
-    println!(
-        "{}       : {}",
-        format!("OS").red().bold().italic(),
-        os_name
-    );
 
     let kernel = fetchit::get_kernel_version();
-    println!(
-        "{}   : {}",
-        format!("KERNEL").magenta().bold().italic(),
-        kernel
-    );
 
     let shell_name = fetchit::get_shell_name();
-    println!(
-        "{}    : {}",
-        format!("SHELL").yellow().bold().italic(),
-        shell_name
-    );
 
     let session = fetchit::get_session_name();
-    println!(
-        "{}  : {}",
-        format!("SESSION").blue().bold().italic(),
-        session
-    );
 
     let uptime = fetchit::get_sys_uptime();
-    println!(
-        "{}   : {}",
-        format!("UPTIME").cyan().bold().italic(),
-        uptime
-    );
 
-    let total_packages = packages::get_num_packages();
-    println!(
-        "{} : {}",
-        format!("PACKAGES").green().bold().italic(),
-        total_packages
-    );
+    let total_packages = packages::get_num_packages().to_string();
+
+    // Create a vector to store the lengths of all the strings
+    let string_length_vector = vec![os_name.len(),
+                                    kernel.len(),
+                                    shell_name.len(),
+                                    session.len(),
+                                    uptime.len(),
+                                    total_packages.len()];
+
+    // Initialize the maximum string length to `0.`
+    let mut max_val = 0;
+
+    // Iterate over the vector, to find the maximum string length.
+    for val in &string_length_vector {
+        if val > &max_val {
+            max_val = *val;
+        }
+    }
+    
+    // Define the length for which the horizontal characters `─`
+    // should be repeated.
+    let final_length = 9 + max_val + 5;
+
+    // The number `12` defines the total characters, upto the output
+    // for each system info value. For example,
+    // ` OS       : ` this will be printed before the name of the OS is
+    // printed, and this text contains 12 characters. The `9` and `5` above
+    // are chosen based on formatting.
+
+    println!("    ______________        ╭{}╮", format!("─").repeat(final_length));
+    println!("   |  __________  |       │ {}       : {}{}{}", format!("OS").red().bold().italic(), os_name, format!(" ").repeat(final_length - 12 - string_length_vector[0]), format!("│"));
+    println!("   | :          : |       │ {}   : {}{}{}", format!("KERNEL").magenta().bold().italic(), kernel, format!(" ").repeat(final_length - 12 - string_length_vector[1]), format!("│"));
+    println!("   | :   Rust   : |       │ {}    : {}{}{}", format!("SHELL").yellow().bold().italic(), shell_name, format!(" ").repeat(final_length - 12 - string_length_vector[2]), format!("│"));
+    println!("   | :__________: |       │ {}  : {}{}{}", format!("SESSION").blue().bold().italic(), session, format!(" ").repeat(final_length - 12 - string_length_vector[3]), format!("│"));
+    println!("   |______________|       │ {}   : {}{}{}", format!("UPTIME").cyan().bold().italic(), uptime, format!(" ").repeat(final_length - 12 - string_length_vector[4]), format!("│"));
+    println!("   \\   =========  \\       │ {} : {}{}{}", format!("PACKAGES").green().bold().italic(), total_packages, format!(" ").repeat(final_length - 12 - string_length_vector[5]), format!("│"));
+    println!("    \\ ==== ____ == \\      ╰{}╯", format!("─").repeat(final_length));
+    println!("     \\_____\\___\\____\\   ");
 }
