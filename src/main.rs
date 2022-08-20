8f517c7853c4275b42020565239d88bd82ccf55d
Ruturajn <nanotiruturaj@gmail.com>
2022-08-20 13:30:30 +0530

    `add`: ascii_arts directory and modify main.rs to read from file

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,8 @@
 // @Date  : 18th August, 2022
 // @Brief : This is a system fetch tool written in Rust.
 
+use std::fs;
+
 use colored::Colorize;
 
 // Bring the functions from `lib.rs`, and 
@@ -53,15 +55,48 @@ fn main() {
     // printed, and this text contains 14 characters. The `11` and `5` above
     // are chosen based on that formatting.
 
+    // let ascii_vec = vec!["    ______________         ",
+    //                      "   |  __________  |        ",
+    //                      "   | :          : |        ",
+    //                      "   | :   Rust   : |        ",
+    //                      "   | :__________: |        ",
+    //                      "   |______________|        ",
+    //                      "   \\   =========   \\     ",
+    //                      "    \\ ==== ____ ==  \\    ",
+    //                      "     \\_____\\___\\_____\\ ",
+    // ];
+    
+    let ascii_string = match fs::read_to_string("/home/ruturajn/Documents/Rust_Projects/fetchit/ascii_assets/laptop.txt") {
+        Ok(x) => x,
+        Err(_) => {
+"     ______________        
+    |  __________  |       
+    | :          : |       
+    | :   Rust   : |       
+    | :__________: |       
+    |______________|       
+    \\   =========   \\      
+     \\ ==== ____ ==  \\     
+      \\_____\\___\\_____\\  ".to_string()
+        }
+    };
+
+    let mut ascii_vec = Vec::new();
+
+    for line in ascii_string.lines() {
+        ascii_vec.push(line);
+    }
+    
+
     println!("");
-    println!("    {}        {}{}{}", format!("_").repeat(14).red(), format!("╭").blue(), format!("─").repeat(final_length).blue(), format!("╮").blue());
-    println!("   {}       {} {}        {}  {}{}{}", format!("|  __________  |").red(), format!("│").blue(), format!("OS").red().bold().italic(), format!("").red(), os_name, format!(" ").repeat(final_length - 14 - string_length_vector[0]), format!("│").blue());
-    println!("   {}       {} {}    {}  {}{}{}", format!("| :          : |").red(), format!("│").blue(), format!("KERNEL").magenta().bold().italic(), format!("").magenta(), kernel, format!(" ").repeat(final_length - 14 - string_length_vector[1]), format!("│").blue());
-    println!("   {}       {} {}     {}  {}{}{}", format!("| :   Rust   : |").red(), format!("│").blue(), format!("SHELL").yellow().bold().italic(), format!("").yellow(), shell_name, format!(" ").repeat(final_length - 14 - string_length_vector[2]), format!("│").blue());
-    println!("   {}       {} {}   {}  {}{}{}", format!("| :__________: |").red(), format!("│").blue(), format!("SESSION").blue().bold().italic(), format!("").blue(), session, format!(" ").repeat(final_length - 14 - string_length_vector[3]), format!("│").blue());
-    println!("   {}       {} {}    {} {}{}{}", format!("|______________|").red(), format!("│").blue(), format!("UPTIME").cyan().bold().italic(), format!("祥").cyan(), uptime, format!(" ").repeat(final_length - 14 - string_length_vector[4]), format!("│").blue());
-    println!("   {}      {} {}  {}  {}{}{}", format!("\\   =========   \\").blue(), format!("│").blue(), format!("PACKAGES").green().bold().italic(), format!("").green(), total_packages, format!(" ").repeat(final_length - 14 - string_length_vector[5]), format!("│").blue());
-    println!("    {}     {}{}{}", format!("\\ ==== ____ ==  \\").blue(), format!("╰").blue(), format!("─").repeat(final_length).blue(), format!("╯").blue());
-    println!("     {}   ", format!("\\_____\\___\\_____\\").blue());
+    println!("{} {}{}{}", ascii_vec[0].red(), format!("╭").blue(), format!("─").repeat(final_length).blue(), format!("╮").blue());
+    println!("{} {} {}        {}  {}{}{}", ascii_vec[1].red(), format!("│").blue(), format!("OS").red().bold().italic(), format!("").red(), os_name, format!(" ").repeat(final_length - 14 - string_length_vector[0]), format!("│").blue());
+    println!("{} {} {}    {}  {}{}{}", ascii_vec[2].red(), format!("│").blue(), format!("KERNEL").magenta().bold().italic(), format!("").magenta(), kernel, format!(" ").repeat(final_length - 14 - string_length_vector[1]), format!("│").blue());
+    println!("{} {} {}     {}  {}{}{}", ascii_vec[3].red(), format!("│").blue(), format!("SHELL").yellow().bold().italic(), format!("").yellow(), shell_name, format!(" ").repeat(final_length - 14 - string_length_vector[2]), format!("│").blue());
+    println!("{} {} {}   {}  {}{}{}", ascii_vec[4].red(), format!("│").blue(), format!("SESSION").blue().bold().italic(), format!("").blue(), session, format!(" ").repeat(final_length - 14 - string_length_vector[3]), format!("│").blue());
+    println!("{} {} {}    {} {}{}{}", ascii_vec[5].red(), format!("│").blue(), format!("UPTIME").cyan().bold().italic(), format!("祥").cyan(), uptime, format!(" ").repeat(final_length - 14 - string_length_vector[4]), format!("│").blue());
+    println!("{} {} {}  {}  {}{}{}", ascii_vec[6].blue(), format!("│").blue(), format!("PACKAGES").green().bold().italic(), format!("").green(), total_packages, format!(" ").repeat(final_length - 14 - string_length_vector[5]), format!("│").blue());
+    println!("{} {}{}{}", ascii_vec[7].blue(), format!("╰").blue(), format!("─").repeat(final_length).blue(), format!("╯").blue());
+    println!("{} ", ascii_vec[8].blue());
     println!("");
 }
