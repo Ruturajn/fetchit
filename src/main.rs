31aae24c480aed47eb7da44722a911b8196e4d50
Ruturajn <nanotiruturaj@gmail.com>
2022-08-20 15:55:17 +0530

    `update` : main.rs and Cargo.toml - Added `clap` crate to handle commadline arguments - Implemented custom ascii art capability using commadline option.

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,21 @@
 // @Author: Ruturajn <nanotiruturaj@gmail.com>
 // @Date  : 18th August, 2022
-// @Brief : This is a system fetch tool written in Rust.
+// @Brief : This is a system fetch tool for Linux written in Rust.
 
 use std::fs;
-
 use colored::Colorize;
+use clap::Parser;
 
 // Bring the functions from `lib.rs`, and 
 // `packages.rs` into scope.
 use fetchit;
 pub mod packages;
 
+
 fn main() {
+    
+    let args = FetchitArgs::parse();
+
     let os_name = match fetchit::get_os_name() {
         Ok(x) => x,
         Err(_) => String::from("Unknown"),
@@ -49,26 +53,7 @@ fn main() {
     // should be repeated.
     let final_length = 11 + max_val + 5;
 
-    // The number `14` defines the total characters, upto the output
-    // for each system info value. For example,
-    // ` OS          ` this will be printed before the name of the OS is
-    // printed, and this text contains 14 characters. The `11` and `5` above
-    // are chosen based on that formatting.
-
-    // let ascii_vec = vec!["    ______________         ",
-    //                      "   |  __________  |        ",
-    //                      "   | :          : |        ",
-    //                      "   | :   Rust   : |        ",
-    //                      "   | :__________: |        ",
-    //                      "   |______________|        ",
-    //                      "   \\   =========   \\     ",
-    //                      "    \\ ==== ____ ==  \\    ",
-    //                      "     \\_____\\___\\_____\\ ",
-    // ];
-    
-    let ascii_string = match fs::read_to_string("/home/ruturajn/Documents/Rust_Projects/fetchit/ascii_assets/laptop.txt") {
-        Ok(x) => x,
-        Err(_) => {
+    let mut ascii_string=
 "     ______________        
     |  __________  |       
     | :          : |       
@@ -77,16 +62,33 @@ fn main() {
     |______________|       
     \\   =========   \\      
      \\ ==== ____ ==  \\     
-      \\_____\\___\\_____\\  ".to_string()
-        }
+      \\_____\\___\\_____\\  ".to_string();
+
+    
+    let custom_ascii_string = match args.file_path {
+        Some(x) => match fs::read_to_string(x) {
+            Ok(y) => y,
+            Err(_) => "Unknown".to_string(),
+        },
+        None => "Unknown".to_string(),
     };
+    
+    if custom_ascii_string != "Unknown".to_string() {
+        ascii_string = custom_ascii_string;
+    }
 
     let mut ascii_vec = Vec::new();
 
     for line in ascii_string.lines() {
         ascii_vec.push(line);
     }
-    
+
+
+    // The number `14` defines the total characters, upto the output
+    // for each system info value. For example,
+    // ` OS          ` this will be printed before the name of the OS is
+    // printed, and this text contains 14 characters. The `11` and `5` above
+    // are chosen based on that formatting.
 
     println!("");
     println!("{} {}{}{}", ascii_vec[0].red(), format!("╭").blue(), format!("─").repeat(final_length).blue(), format!("╮").blue());
@@ -99,4 +101,27 @@ fn main() {
     println!("{} {}{}{}", ascii_vec[7].blue(), format!("╰").blue(), format!("─").repeat(final_length).blue(), format!("╯").blue());
     println!("{} ", ascii_vec[8].blue());
     println!("");
+}
+
+#[derive(Debug, Parser)]
+#[clap(author, version, about, long_about = None)]
+struct FetchitArgs {
+   /// Color for the top part of the laptop
+   /// : black, red, yellow, blue, magenta, cyan, white
+   #[clap(short, long, value_parser)]
+   top_border_color: Option<String>,
+
+   /// Color for the bottom part of the laptop
+   /// : black, red, yellow, blue, magenta, cyan, white
+   #[clap(short, long, value_parser)]
+   bottom_border_color: Option<String>,
+
+   /// Color for the box
+   /// : black, red, yellow, blue, magenta, cyan, white
+   #[clap(short, long, value_parser)]
+   outer_box_color: Option<String>,
+
+   /// File path for the ascii text file 
+   #[clap(short, long, parse(from_os_str))]
+   file_path: Option<std::path::PathBuf>,
 }
